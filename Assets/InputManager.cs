526d9856992069772ad7e6b22465887ed46e3bd8
nakagawa321 <54922044+nakagawa321@users.noreply.github.com>
2019-09-16 17:42:10 +0900

    commit
    add animation

---
+++ Assets/InputManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    private float flap = 500f;
+    private float speed = 5.0f;
+    private bool jump = false;
+    private Vector2 logPosition = new Vector2(0.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody2Dを取得
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetInteger("is_state", 0); // 何もない状態
+
+        // 移動
+        if (Input.GetKey("right"))
+        {
+            animator.SetInteger("is_state", 1);
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            animator.SetInteger("is_state", 1);
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        // ジャンプ
+        if (Input.GetKeyDown("space") && !jump)
+        {
+            animator.SetInteger("is_state", 2);
+            rb.AddForce(Vector2.up * flap);
+            jump = true;
+        }
+
+        // 落下中か
+        if(transform.position.y < logPosition.y)
+        {
+            animator.SetInteger("is_state", 3);
+        }
+        logPosition.y = transform.position.y;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        jump = false;        
+    }
+
+}
